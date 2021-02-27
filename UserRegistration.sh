a2a2b63d375543228f5f1c4a194e10726f17e7a3
Prasanna-Nanda <prasannanandakumar92@gmail.com>
2021-02-27 23:10:25 +0530

    Added UC2_LastName

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,3 +1,4 @@
+  GNU nano 4.9.3                 UserRegistration.sh
 import java.sql.SQLOutput;
 import java.util.Scanner;
 import java.util.regex.Pattern;
@@ -11,6 +12,13 @@ public class UserRegistrationMain implements UserRegisterationInterface {
         Pattern firstNameRegExp=Pattern.compile("(^[A-Z]{1})[a-z]{2,}");
         String userInput=scanner.next();
 
+  System.out.println("Enter Last Name:");
+        Scanner scanner1=new Scanner(System.in);
+        String userInputLastName=scanner1.next();
+
+        Pattern lastNameRegExp=Pattern.compile("([A-Z]{1}[a-z]{2,}\s{0,}[A-Z]{1}[a-z]{2,})");
+
     }
 
 }
+
